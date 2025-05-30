b273631b73f7ce7f92cd78e3549e363445299bd8
aymanfouad123 <aymanfouad033@gmail.com>
2025-05-30 17:45:59 -0400

    feat: add and show items

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,22 @@ import "./App.css";
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleAdd = () => {
+    setTodos([...todos, { id: Date.now(), todo, isCompleted: false }]);
+    setTodo("");
+    console.log(todos);
+  };
+
+  const handleEdit = () => {};
+  const handleDelete = () => {};
+
   return (
     <>
       <div className="flex flex-col min-h-screen">
@@ -12,31 +28,49 @@ function App() {
           <div className="addTodo">
             <h2 className="text-lg font-bold">Add Todo</h2>
             <div className="inputTodo flex gap-2 my-3">
-              <input type="text" className="bg-gray-100 rounded-md" />
-              <button className="btn bg-gray-600 hover:bg-gray-900">Add</button>
+              <input
+                onChange={handleChange}
+                value={todo}
+                type="text"
+                className="bg-gray-100 rounded-md"
+              />
+              <button
+                onClick={handleAdd}
+                className="btn bg-gray-600 hover:bg-gray-900"
+              >
+                Add
+              </button>
             </div>
           </div>
 
           <h2 className="text-lg font-bold mt-9">Your Todo's</h2>
           <div className="todoList mt-3">
-            <div className="card border-2 border-dashed border-purple-400 bg-gray-200 flex justify-between items-center p-4 rounded-md">
-              <div className="todoDesc text-left max-w-4/5">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel ad
-                aliquam animi nemo Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Consequuntur alias hic voluptatum,
-                voluptatibus, consequatur impedit quam pariatur, aliquid error
-                blanditiis quas est totam officiis eius rerum consectetur
-                adipisci distinctio et.
-              </div>
-              <div className="buttons flex gap-3">
-                <button className="btn bg-gray-600 hover:bg-gray-900">
-                  Edit
-                </button>
-                <button className="btn bg-purple-600 hover:bg-purple-900">
-                  Delete
-                </button>
-              </div>
-            </div>
+            {todos.map((items) => {
+              return (
+                <div
+                  key={items.id}
+                  className="card border-2 border-dashed border-purple-400 bg-gray-200 flex justify-between items-center p-4 rounded-md"
+                >
+                  <div className={items.isCompleted ? "line-through" : ""}>
+                    {items.todo}
+                  </div>
+                  <div className="buttons flex gap-3">
+                    <button
+                      onClick={handleEdit}
+                      className="btn bg-gray-600 hover:bg-gray-900"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={handleDelete}
+                      className="btn bg-purple-600 hover:bg-purple-900"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
